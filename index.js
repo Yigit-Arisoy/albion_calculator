c3c804996c7769e7ca219e77302bc9fdb4a571bd
Yigit-Arisoy <arisoy.yigit@gmail.com>
2022-07-26 16:26:49 +0300

    Added Functionality
    +Added functionality for the rest of the resources.
    +Rock option is temporarily disabled as the refining process is not implemented correctly.

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ buttonFiber.addEventListener("click", ButtonFiber);
 let buttonOre = document.getElementById("ore-button");
 buttonOre.addEventListener("click", ButtonOre);
 
-let buttonRock = document.getElementById("rock-button");
-buttonRock.addEventListener("click", ButtonRock);
+// let buttonRock = document.getElementById("rock-button");
+// buttonRock.addEventListener("click", ButtonRock);
 
 function ButtonHide() {
   Array.from(buttonHide.parentElement.children).forEach((button) => {
@@ -140,7 +140,7 @@ function ButtonHide() {
 }
 
 function ButtonWood() {
-  Array.from(buttonHide.parentElement.children).forEach((button) => {
+  Array.from(buttonWood.parentElement.children).forEach((button) => {
     button.style.backgroundColor = "#0a0a0af2";
     button.style.color = "white";
     button.style.borderColor = "white";
@@ -150,11 +150,97 @@ function ButtonWood() {
   buttonWood.style.color = "#0a0a0af2";
   buttonWood.style.borderColor = "black";
 
-  changeBackground("wood-background");
+  const wood_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T4_WOOD_LEVEL1@1,T5_WOOD_LEVEL1@1,T6_WOOD_LEVEL1@1,T7_WOOD_LEVEL1@1,T8_WOOD_LEVEL1@1,T4_WOOD_LEVEL2@2,T5_WOOD_LEVEL2@2,T6_WOOD_LEVEL2@2,T7_WOOD_LEVEL2@2,T8_WOOD_LEVEL2@2,T4_WOOD_LEVEL3@3,T5_WOOD_LEVEL3@3,T6_WOOD_LEVEL3@3,T7_WOOD_LEVEL3@3,T8_WOOD_LEVEL3@3,T4_WOOD,T5_WOOD,T6_WOOD,T7_WOOD,T8_WOOD?locations=FortSterling"
+  ).then((res) => res.json());
+
+  const planks_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T3_PLANKS,T4_PLANKS_LEVEL1@1,T5_PLANKS_LEVEL1@1,T6_PLANKS_LEVEL1@1,T7_PLANKS_LEVEL1@1,T8_PLANKS_LEVEL1@1,T4_PLANKS_LEVEL2@2,T5_PLANKS_LEVEL2@2,T6_PLANKS_LEVEL2@2,T7_PLANKS_LEVEL2@2,T8_PLANKS_LEVEL2@2,T4_PLANKS_LEVEL3@3,T5_PLANKS_LEVEL3@3,T6_PLANKS_LEVEL3@3,T7_PLANKS_LEVEL3@3,T8_PLANKS_LEVEL3@3,T4_PLANKS,T5_PLANKS,T6_PLANKS,T7_PLANKS,T8_PLANKS?locations=FortSterling"
+  ).then((res) => res.json());
+
+  Promise.all([wood_fetch, planks_fetch]).then((res) => {
+    console.log(res);
+
+    let output = "";
+    let profit = [];
+    let rr = 1 - 0.54;
+
+    for (let i = 0; i < 4; i++) {
+      let woodCost = res[0][i].sell_price_min;
+      let planksCost = res[1][0].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost = (woodCost * 2 + planksCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i + 1].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][0].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T4_${i}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${woodCost}</td>
+      <td>${planksCost}</td>
+      </tr>
+       `;
+    }
+
+    for (let i = 4; i < 20; i++) {
+      let woodCost = res[0][i].sell_price_min;
+      let planksCost = res[1][i - 3].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost =
+        (woodCost * Math.min(2 + parseInt(i / 4), 5) + planksCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i - 3].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][i + 1].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T${4 + parseInt(i / 4)}_${i % 4}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${woodCost}</td>
+      <td>${planksCost}</td>
+      </tr>
+       `;
+    }
+
+    document.querySelector("tbody").innerHTML = output;
+    changeBackground("wood-background");
+  });
 }
 
 function ButtonFiber() {
-  Array.from(buttonHide.parentElement.children).forEach((button) => {
+  Array.from(buttonFiber.parentElement.children).forEach((button) => {
     button.style.backgroundColor = "#0a0a0af2";
     button.style.color = "white";
     button.style.borderColor = "white";
@@ -164,11 +250,97 @@ function ButtonFiber() {
   buttonFiber.style.color = "#0a0a0af2";
   buttonFiber.style.borderColor = "black";
 
-  changeBackground("fiber-background");
+  const fiber_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T4_FIBER_LEVEL1@1,T5_FIBER_LEVEL1@1,T6_FIBER_LEVEL1@1,T7_FIBER_LEVEL1@1,T8_FIBER_LEVEL1@1,T4_FIBER_LEVEL2@2,T5_FIBER_LEVEL2@2,T6_FIBER_LEVEL2@2,T7_FIBER_LEVEL2@2,T8_FIBER_LEVEL2@2,T4_FIBER_LEVEL3@3,T5_FIBER_LEVEL3@3,T6_FIBER_LEVEL3@3,T7_FIBER_LEVEL3@3,T8_FIBER_LEVEL3@3,T4_FIBER,T5_FIBER,T6_FIBER,T7_FIBER,T8_FIBER?locations=Lymhurst"
+  ).then((res) => res.json());
+
+  const cloth_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T3_CLOTH,T4_CLOTH_LEVEL1@1,T5_CLOTH_LEVEL1@1,T6_CLOTH_LEVEL1@1,T7_CLOTH_LEVEL1@1,T8_CLOTH_LEVEL1@1,T4_CLOTH_LEVEL2@2,T5_CLOTH_LEVEL2@2,T6_CLOTH_LEVEL2@2,T7_CLOTH_LEVEL2@2,T8_CLOTH_LEVEL2@2,T4_CLOTH_LEVEL3@3,T5_CLOTH_LEVEL3@3,T6_CLOTH_LEVEL3@3,T7_CLOTH_LEVEL3@3,T8_CLOTH_LEVEL3@3,T4_CLOTH,T5_CLOTH,T6_CLOTH,T7_CLOTH,T8_CLOTH?locations=Lymhurst"
+  ).then((res) => res.json());
+
+  Promise.all([fiber_fetch, cloth_fetch]).then((res) => {
+    console.log(res);
+
+    let output = "";
+    let profit = [];
+    let rr = 1 - 0.54;
+
+    for (let i = 0; i < 4; i++) {
+      let fiberCost = res[0][i].sell_price_min;
+      let clothCost = res[1][0].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost = (fiberCost * 2 + clothCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i + 1].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][0].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T4_${i}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${fiberCost}</td>
+      <td>${clothCost}</td>
+      </tr>
+       `;
+    }
+
+    for (let i = 4; i < 20; i++) {
+      let fiberCost = res[0][i].sell_price_min;
+      let clothCost = res[1][i - 3].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost =
+        (fiberCost * Math.min(2 + parseInt(i / 4), 5) + clothCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i - 3].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][i + 1].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T${4 + parseInt(i / 4)}_${i % 4}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${fiberCost}</td>
+      <td>${clothCost}</td>
+      </tr>
+       `;
+    }
+
+    document.querySelector("tbody").innerHTML = output;
+    changeBackground("fiber-background");
+  });
 }
 
 function ButtonOre() {
-  Array.from(buttonHide.parentElement.children).forEach((button) => {
+  Array.from(buttonOre.parentElement.children).forEach((button) => {
     button.style.backgroundColor = "#0a0a0af2";
     button.style.color = "white";
     button.style.borderColor = "white";
@@ -178,11 +350,97 @@ function ButtonOre() {
   buttonOre.style.color = "#0a0a0af2";
   buttonOre.style.borderColor = "black";
 
-  changeBackground("ore-background");
+  const ore_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T4_ORE_LEVEL1@1,T5_ORE_LEVEL1@1,T6_ORE_LEVEL1@1,T7_ORE_LEVEL1@1,T8_ORE_LEVEL1@1,T4_ORE_LEVEL2@2,T5_ORE_LEVEL2@2,T6_ORE_LEVEL2@2,T7_ORE_LEVEL2@2,T8_ORE_LEVEL2@2,T4_ORE_LEVEL3@3,T5_ORE_LEVEL3@3,T6_ORE_LEVEL3@3,T7_ORE_LEVEL3@3,T8_ORE_LEVEL3@3,T4_ORE,T5_ORE,T6_ORE,T7_ORE,T8_ORE?locations=Thetford"
+  ).then((res) => res.json());
+
+  const metalBar_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T3_METALBAR,T4_METALBAR_LEVEL1@1,T5_METALBAR_LEVEL1@1,T6_METALBAR_LEVEL1@1,T7_METALBAR_LEVEL1@1,T8_METALBAR_LEVEL1@1,T4_METALBAR_LEVEL2@2,T5_METALBAR_LEVEL2@2,T6_METALBAR_LEVEL2@2,T7_METALBAR_LEVEL2@2,T8_METALBAR_LEVEL2@2,T4_METALBAR_LEVEL3@3,T5_METALBAR_LEVEL3@3,T6_METALBAR_LEVEL3@3,T7_METALBAR_LEVEL3@3,T8_METALBAR_LEVEL3@3,T4_METALBAR,T5_METALBAR,T6_METALBAR,T7_METALBAR,T8_METALBAR?locations=Thetford"
+  ).then((res) => res.json());
+
+  Promise.all([ore_fetch, metalBar_fetch]).then((res) => {
+    console.log(res);
+
+    let output = "";
+    let profit = [];
+    let rr = 1 - 0.54;
+
+    for (let i = 0; i < 4; i++) {
+      let oreCost = res[0][i].sell_price_min;
+      let metalBarCost = res[1][0].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost = (oreCost * 2 + metalBarCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i + 1].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][0].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T4_${i}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${oreCost}</td>
+      <td>${metalBarCost}</td>
+      </tr>
+       `;
+    }
+
+    for (let i = 4; i < 20; i++) {
+      let oreCost = res[0][i].sell_price_min;
+      let metalBarCost = res[1][i - 3].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost =
+        (oreCost * Math.min(2 + parseInt(i / 4), 5) + metalBarCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i - 3].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][i + 1].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T${4 + parseInt(i / 4)}_${i % 4}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${oreCost}</td>
+      <td>${metalBarCost}</td>
+      </tr>
+       `;
+    }
+
+    document.querySelector("tbody").innerHTML = output;
+    changeBackground("ore-background");
+  });
 }
 
 function ButtonRock() {
-  Array.from(buttonHide.parentElement.children).forEach((button) => {
+  Array.from(buttonRock.parentElement.children).forEach((button) => {
     button.style.backgroundColor = "#0a0a0af2";
     button.style.color = "white";
     button.style.borderColor = "white";
@@ -192,5 +450,91 @@ function ButtonRock() {
   buttonRock.style.color = "#0a0a0af2";
   buttonRock.style.borderColor = "black";
 
-  changeBackground("rock-background");
+  const rock_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T4_ROCK_LEVEL1@1,T5_ROCK_LEVEL1@1,T6_ROCK_LEVEL1@1,T7_ROCK_LEVEL1@1,T8_ROCK_LEVEL1@1,T4_ROCK_LEVEL2@2,T5_ROCK_LEVEL2@2,T6_ROCK_LEVEL2@2,T7_ROCK_LEVEL2@2,T8_ROCK_LEVEL2@2,T4_ROCK_LEVEL3@3,T5_ROCK_LEVEL3@3,T6_ROCK_LEVEL3@3,T7_ROCK_LEVEL3@3,T8_ROCK_LEVEL3@3,T4_ROCK,T5_ROCK,T6_ROCK,T7_ROCK,T8_ROCK?locations=Bridgewatch"
+  ).then((res) => res.json());
+
+  const stoneBlock_fetch = fetch(
+    "https://www.albion-online-data.com/api/v2/stats/prices/T3_STONEBLOCK,T4_STONEBLOCK_LEVEL1@1,T5_STONEBLOCK_LEVEL1@1,T6_STONEBLOCK_LEVEL1@1,T7_STONEBLOCK_LEVEL1@1,T8_STONEBLOCK_LEVEL1@1,T4_STONEBLOCK_LEVEL2@2,T5_STONEBLOCK_LEVEL2@2,T6_STONEBLOCK_LEVEL2@2,T7_STONEBLOCK_LEVEL2@2,T8_STONEBLOCK_LEVEL2@2,T4_STONEBLOCK_LEVEL3@3,T5_STONEBLOCK_LEVEL3@3,T6_STONEBLOCK_LEVEL3@3,T7_STONEBLOCK_LEVEL3@3,T8_STONEBLOCK_LEVEL3@3,T4_STONEBLOCK,T5_STONEBLOCK,T6_STONEBLOCK,T7_STONEBLOCK,T8_STONEBLOCK?locations=Bridgewatch"
+  ).then((res) => res.json());
+
+  Promise.all([rock_fetch, stoneBlock_fetch]).then((res) => {
+    console.log(res);
+
+    let output = "";
+    let profit = [];
+    let rr = 1 - 0.54;
+
+    for (let i = 0; i < 4; i++) {
+      let rockCost = res[0][i].sell_price_min;
+      let stoneBlockCost = res[1][0].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost = (rockCost * 2 + stoneBlockCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i + 1].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][0].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T4_${i}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${rockCost}</td>
+      <td>${stoneBlockCost}</td>
+      </tr>
+       `;
+    }
+
+    for (let i = 4; i < 20; i++) {
+      let rockCost = res[0][i].sell_price_min;
+      let stoneBlockCost = res[1][i - 3].sell_price_min;
+      let sellPrice = res[1][i + 1].sell_price_min;
+
+      let cost =
+        (rockCost * Math.min(2 + parseInt(i / 4), 5) + stoneBlockCost) * rr;
+
+      profit[i] = sellPrice - cost;
+
+      let isLoss = "";
+
+      if (profit[i] < 0) {
+        isLoss = "class = loss";
+      } else {
+        isLoss = "class = profit";
+      }
+
+      let oldestDate = Math.max(
+        new Date(res[1][i - 3].sell_price_min_date),
+        new Date(res[0][i].sell_price_min_date),
+        new Date(res[1][i + 1].sell_price_min_date)
+      );
+
+      output += `
+      <tr>
+      <td>T${4 + parseInt(i / 4)}_${i % 4}</td>
+      <td ${isLoss}>${parseInt(profit[i])}</td>
+      <td>${updateTime(oldestDate)}</td>
+      <td>${rockCost}</td>
+      <td>${stoneBlockCost}</td>
+      </tr>
+       `;
+    }
+
+    document.querySelector("tbody").innerHTML = output;
+    changeBackground("rock-background");
+  });
 }
